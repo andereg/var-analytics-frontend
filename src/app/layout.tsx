b9a3e4d3b10ef607c2d28cb8ebdc4981a4cc7d65
Anderegg Raphael <raphaelanderegg15@gmail.com>
2026-03-30 22:58:49 +0200

    changed matomo tag to yanns

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,18 +26,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script id="matomo-init" strategy="beforeInteractive">
             {`
             var _paq = window._paq = window._paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="https://ambatukam.matomo.cloud/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '1']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true;
-              g.src='https://cdn.matomo.cloud/ambatukam.matomo.cloud/matomo.js';
-              s.parentNode.insertBefore(g,s);
-            })();
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//46.225.49.241:8082/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '4']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
           `}
         </Script>
         <TopicProvider>
